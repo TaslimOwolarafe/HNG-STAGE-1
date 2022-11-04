ac76d1c54a854651e78dda767867cbcfc305eafb
TaslimOwolarafe <owolarafetaslim@gmail.com>
2022-11-04 10:15:18 +0100

    implemented gpt-3

--- stage1_endpoint/views.py
+++ stage1_endpoint/views.py
@@ -4,6 +4,26 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 # Create your views here.
 
+import os
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def get_operation_value(text):
+    response = openai.Completion.create(
+    model="text-davinci-002",
+    prompt=f"Can you please add the following numbers together - 13 and 25: addition\ncan you subtract 4 from 5: subtraction\ncan you add 4 and 5: addition\ncan you multiply 5 and 6: multiplication\nCan you please multiply the following numbers together - 13 and 25: multiplication\nCan you please add the following numbers together - 13 and 34: addition\n{text}: ",
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+    return response["choices"][0]['text'][1::].split()[0]
+
 def index(request, *args, **kwargs):
     return JsonResponse({
             "slackUsername" : 'TaslimOwolarafe',
@@ -15,22 +35,23 @@ def index(request, *args, **kwargs):
 @api_view(['POST', 'GET'])
 def operationView(request, *args, **kwargs):
     response = {"slackUsername" : 'TaslimOwolarafe'}
-    print(request.data)
     if request.method == 'POST':
+        
         operation = request.data.get('operation_type')
         x = int(request.data.get('x'))
         y = int(request.data.get('y'))
         result = None
-        if operation == 'addition':
+        operation_type = get_operation_value(operation)
+        if operation or operation_type == 'addition':
             result = x + y
-        elif operation == 'subtraction':
+        elif operation or operation_type == 'subtraction':
             result = x - y
-        elif operation == 'multiplication':
+        elif operation or operation_type == 'multiplication':
             result = x * y
         
         response = {
             "slackUsername" : 'TaslimOwolarafe',
             "result" : result,
-            "operation_type" : operation
+            "operation_type" : operation_type
         }
     return Response(response)
